37e833924093dc710c67aff85d722232725af6c1
yesh1anu <48102124+yesh1anu@users.noreply.github.com>
2019-03-01 20:06:52 +0530

    Add files via upload

---
+++ smartfarming.ino
@@ -0,0 +1,23 @@
+int ACWATERPUMP = 13; 
+int sensor = A0;
+int val=0; 
+void setup() {
+  
+pinMode(13,OUTPUT);
+pinMode(A0,INPUT); 
+Serial.begin(9600);
+}
+
+void loop() {
+val = analogRead(A0);
+if(val ==LOW)
+{
+  analogWrite(13,LOW); 
+}
+else
+{
+analogWrite(13,HIGH); 
+}
+delay(400);
+}
+
